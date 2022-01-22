a21c9c6bc8a8f0f3cbb89ae3fabdf92ae57bcaac
HeyMaroka <marisantl@hotmail.com>
2022-01-22 15:54:23 -0300

    update project

---
+++ script.js
@@ -0,0 +1,50 @@
+function mostrarMessagem() {console.log("Vou descrever sobre Quem eu sou, Minha idade e meu estilo Musical favorito" )}
+mostrarMessagem()
+
+
+function MeuNome (){
+    console.log("Marina");
+}
+MeuNome ();
+
+function dados (nome){
+    console.log (`Meu nome é ${nome}.`);
+}
+dados (`Marina`);
+
+function introducao (nome, idade, Meuestilomusicalfavorito){
+    console.log(`Meu nome é ${nome}, Minha idade é ${idade} Meu estilo musical favorito é ${musical}.`);
+}
+introducao(`Marina`, `20`, `Ecletico`);
+{
+
+function filmefavoritoemusicafavorita (){
+    console.log("Filme e Musica");
+}
+filmefavoritoemusicafavorita ();
+
+function dados (nome){
+    console.log (`filme favorito e musica favorita ${nome}.`);
+}
+dados (`Filme e Musica`);
+
+function introducao ( FilmeeMusica){
+    console.log(`meu filme favorito é ${nome}, minha musica favorita é ${musica}.`);
+}
+introducao(`Acho que não tenho um filme favorito mas diria com certeza o que mais assisti na vida foi a saga harry potter`,`Crazy Gnars barkley`);
+{
+    #include <stdio.h>
+triplo (intnumero)
+{
+    return numero*3;
+}
+intmain()
+{
+    intnum;
+    printf("3");
+    scanf("%d"&intnum);
+    printf("\nO tripo de %d = %d\3\3", num, triplo(num));
+    getch();
+}
+
+
